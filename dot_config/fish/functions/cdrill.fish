ce229057d909681a516030398f14768ad6a7e357
Josh Kunz <jkz@stripe.com>
2025-11-20 08:22:26 -0800

    Add git-compatible cdrill

--- dot_config/fish/functions/cdrill.fish
+++ dot_config/fish/functions/cdrill.fish
@@ -1,3 +1,8 @@
 function cdrill
-    cd (fd --type directory | fzf)
+    if in_git_dir
+        cd (git ls-files | rev | cut -d/ -f2- | rev | awk '!a[$0]++' | fzf)
+    else
+        cd (fd --type directory | fzf)
+    end
+
 end
